fde848801a5c15485d6855c85586380160670df0
Alex Haslehurst <alex.haslehurst@gmail.com>
2021-05-05 17:33:39 +0100

    fix local typings & removed unused flash middleware (#45)

--- server/@types/express/index.d.ts
+++ server/@types/express/index.d.ts
@@ -1,28 +1,5 @@
-export default {}
-
-declare module 'express-session' {
-  // Declare that the session will potentially contain these additional fields
-  interface SessionData {
-    returnTo: string
-    nowInMinutes: number
-  }
-}
-
-export declare global {
-  interface UserPrincipal {
-    token: string
-    name: string
-    displayName: string
-    username: string
-    active: boolean
-    authSource: string
-    userId: string
-    uuid: string
-  }
-  namespace Express {
-    interface Request {
-      verified?: boolean
-      user: UserPrincipal
-    }
+declare namespace Express {
+  interface Request {
+    user: UserPrincipal
   }
 }
