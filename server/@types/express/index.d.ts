2babf4256210f98d401d6a5a8d81a7c980d89a05
Alex Haslehurst <alex.haslehurst@gmail.com>
2021-05-06 09:45:06 +0100

    fixed bad typings (#46)

--- server/@types/express/index.d.ts
+++ server/@types/express/index.d.ts
@@ -1,3 +1,16 @@
+/// <reference types="express" />
+
+interface UserPrincipal {
+  token: string
+  name: string
+  displayName: string
+  username: string
+  active: boolean
+  authSource: string
+  userId: string
+  uuid: string
+}
+
 declare namespace Express {
   interface Request {
     user: UserPrincipal
