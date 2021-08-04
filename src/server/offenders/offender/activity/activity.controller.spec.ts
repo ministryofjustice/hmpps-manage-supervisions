03505d8dc1d529a65d690cddbd5db935649032a7
dnyanesh.sonwane <dnyanesh.sonwane@digital.justice.gov.uk>
2021-08-04 13:26:51 +0100

    Added controller unit test

--- src/server/offenders/offender/activity/activity.controller.spec.ts
+++ src/server/offenders/offender/activity/activity.controller.spec.ts
@@ -1,9 +1,13 @@
 import { Test } from '@nestjs/testing'
 import { ActivityController } from './activity.controller'
-import { SinonStubbedInstance, createStubInstance } from 'sinon'
+import { createStubInstance, SinonStubbedInstance } from 'sinon'
 import { OffenderService } from '../offender.service'
 import { ActivityService } from './activity.service'
-import { MockLinksModule } from '../../../common/links/links.mock'
+import { fakeBreadcrumbs, MockLinksModule } from '../../../common/links/links.mock'
+import { BreadcrumbType } from '../../../common/links'
+import { fakeOffenderDetailSummary } from '../../../community-api/community-api.fake'
+import { getDisplayName } from '../../../util'
+import { fakeCommunicationActivityLogEntry } from './activity.fake'
 
 describe('ActivityController', () => {
   let subject: ActivityController
@@ -29,4 +33,36 @@ describe('ActivityController', () => {
   it('should be defined', () => {
     expect(subject).toBeDefined()
   })
+
+  it('gets communication', async () => {
+    const offender = havingOffenderSummary()
+
+    const contact = fakeCommunicationActivityLogEntry()
+    activityService.getCommunicationContact.withArgs('some-crn', 111).resolves(contact)
+
+    const observed = await subject.getCommunication('some-crn', 111)
+    const displayName = getDisplayName(offender)
+    expect(observed).toEqual({
+      displayName,
+      breadcrumbs: fakeBreadcrumbs(BreadcrumbType.OtherCommunication, {
+        crn: 'some-crn',
+        offenderName: displayName,
+        entityName: contact.name,
+        id: 111,
+      }),
+      contact,
+    })
+  })
+
+  function havingOffenderSummary() {
+    const offender = fakeOffenderDetailSummary({
+      otherIds: { crn: 'some-crn', pncNumber: 'some-prn' },
+      firstName: 'Liz',
+      middleNames: ['Danger'],
+      surname: 'Haggis',
+      preferredName: 'Bob',
+    })
+    offenderService.getOffenderSummary.withArgs('some-crn').resolves(offender)
+    return offender
+  }
 })
--- src/server/offenders/offender/activity/activity.fake.ts
+++ src/server/offenders/offender/activity/activity.fake.ts
@@ -3,7 +3,7 @@ import { FakeAppointmentDetailOptions } from '../../../community-api/community-a
 import * as faker from 'faker'
 import { ContactTypeCategory } from '../../../config'
 import { DateTime } from 'luxon'
-import { ActivityLogEntry } from './activity.types'
+import { ActivityLogEntry, CommunicationActivityLogEntry } from './activity.types'
 import { AppointmentRequirementDetail } from '../../../community-api/client'
 
 export const fakeActivityLogEntry = fake<ActivityLogEntry, FakeAppointmentDetailOptions>(
@@ -47,3 +47,20 @@ export const fakeActivityLogEntry = fake<ActivityLogEntry, FakeAppointmentDetail
     }
   },
 )
+export const fakeCommunicationActivityLogEntry = fake<CommunicationActivityLogEntry>(() => ({
+  id: faker.datatype.number(),
+  start: DateTime.fromJSDate(faker.datatype.datetime()),
+  notes: faker.company.bs(),
+  name: faker.company.bs(),
+  tags: [],
+  links: {
+    view: faker.internet.url(),
+    addNotes: faker.internet.url(),
+  },
+  category: faker.company.bs(),
+  typeName: faker.company.bs(),
+  sensitive: faker.datatype.boolean(),
+  type: ContactTypeCategory.Communication,
+  lastUpdatedDateTime: DateTime.fromJSDate(faker.datatype.datetime()),
+  lastUpdatedBy: faker.datatype.string(),
+}))
