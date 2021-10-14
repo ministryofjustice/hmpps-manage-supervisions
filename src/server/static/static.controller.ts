a7aa1dc30dbf2a44d89a9b2f4556b12fc9f2eb57
Theodor Vararu <theo@vararu.org>
2021-10-14 14:43:40 +0300

    Add static controller and initial accessibility statement (#327)
    The statement is based off of the one from the Prepare a case service:
    https://github.com/ministryofjustice/prepare-a-case/blob/310446cb9f0929e71f3940ad2c51d2becf387785/server/views/accessibility-statement.njk
    
    Plus the guidance from GOV.UK's Service Manual:
    https://www.gov.uk/service-manual/helping-people-to-use-your-service/publishing-information-about-your-services-accessibility

---
+++ src/server/static/static.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get, Render } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { Config, ServerConfig } from '../config'
+
+@Controller()
+export class StaticController {
+  constructor(private readonly config: ConfigService<Config>) {}
+
+  @Get('accessibility-statement')
+  @Render('static/accessibility-statement')
+  async getAccessibilityStatement() {
+    const { domain } = this.config.get<ServerConfig>('server')
+    return { links: { domain } }
+  }
+}
