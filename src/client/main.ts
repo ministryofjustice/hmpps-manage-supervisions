48bdb6c9cab588016487ad534a54fed9f01d5ed7
Theodor Vararu <theo@vararu.org>
2021-10-20 14:31:12 +0100

    Remove timepicker and dependency on jQuery and jQuery-ui

--- src/client/main.ts
+++ src/client/main.ts
@@ -1,26 +1,21 @@
 import './sass/application.sass'
-import * as $ from 'jquery'
-import 'jquery-ui'
 import * as govuk from 'govuk-frontend'
 import '@ministryofjustice/frontend'
-import 'timepicker'
 import * as accessibleAutocomplete from 'accessible-autocomplete'
 
 govuk.initAll()
 
-$(() => {
+document.addEventListener('DOMContentLoaded', () => {
   /**
    * Progressively enhance any <select data-enhance="accessible-autocomplete"> element.
    */
-  $('select[data-enhance="accessible-autocomplete"]').each((i, x) => {
+  document.querySelectorAll('select[data-enhance="accessible-autocomplete"]').forEach(x => {
     accessibleAutocomplete.enhanceSelectElement({
-      selectElement: x,
+      selectElement: x as HTMLElement,
       showAllValues: true,
       defaultValue: '',
       autoselect: false,
       displayMenu: 'overlay',
     })
   })
-
-  $('.time-input').timepicker({ minTime: '9:00AM', maxTime: '4:45PM', step: 15 })
 })
