800e7ec20b9f40e21111578583d5f99be2e63b09
Theodor Vararu <theo@vararu.org>
2021-10-22 17:17:25 +0300

    Use accessible-autocomplete for time inputs  (#356)
    * Remove timepicker and dependency on jQuery and jQuery-ui
    
    * Update accessible autocomplete type definition file
    
    Add definition for the default exported function.
    
    * Use accessible-autocomplete for time inputs

--- src/client/main.ts
+++ src/client/main.ts
@@ -1,20 +1,45 @@
 import './sass/application.sass'
-import * as $ from 'jquery'
-import 'jquery-ui'
 import * as govuk from 'govuk-frontend'
 import '@ministryofjustice/frontend'
-import 'timepicker'
 import * as accessibleAutocomplete from 'accessible-autocomplete'
+import flatMap = require('lodash/flatMap')
 
 govuk.initAll()
 
-$(() => {
+const times = flatMap([9, 10, 11, 12, 13, 14, 15, 16], x => {
+  const hrs = x > 12 ? x - 12 : x
+  const suffix = x >= 12 ? 'pm' : 'am'
+  return [`${hrs}:00${suffix}`, `${hrs}:15${suffix}`, `${hrs}:30${suffix}`, `${hrs}:45${suffix}`]
+})
+
+const enhanceInputElement = configurationOptions => {
+  if (configurationOptions.id === undefined) {
+    configurationOptions.id = configurationOptions.element.id
+  }
+
+  configurationOptions.defaultValue = configurationOptions.element.value
+  configurationOptions.name = configurationOptions.element.name
+
+  const element = document.createElement('div')
+  element.style.maxWidth = '110px'
+
+  configurationOptions.element.parentNode.insertBefore(element, configurationOptions.element)
+
+  accessibleAutocomplete({
+    ...configurationOptions,
+    element,
+  })
+
+  configurationOptions.element.remove()
+}
+
+document.addEventListener('DOMContentLoaded', () => {
   /**
    * Progressively enhance any <select data-enhance="accessible-autocomplete"> element.
    */
-  $('select[data-enhance="accessible-autocomplete"]').each((i, x) => {
+  Array.prototype.forEach.call(document.querySelectorAll('select[data-enhance="accessible-autocomplete"]'), x => {
     accessibleAutocomplete.enhanceSelectElement({
-      selectElement: x,
+      selectElement: x as HTMLElement,
       showAllValues: true,
       defaultValue: '',
       autoselect: false,
@@ -22,5 +47,12 @@ $(() => {
     })
   })
 
-  $('.time-input').timepicker({ minTime: '9:00AM', maxTime: '4:45PM', step: 15 })
+  Array.prototype.forEach.call(document.querySelectorAll('.time-input'), x => {
+    enhanceInputElement({
+      element: x,
+      source: times,
+      showAllValues: true,
+      showNoOptionsFound: false,
+    })
+  })
 })
