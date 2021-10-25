e742cb80f57229b453f4e5048c9fb3aba20ac51f
Alex Haslehurst <alex.haslehurst@gmail.com>
2021-10-25 14:15:28 +0100

    Attempt to fix unnecessary retries (#368)

--- src/server/common/rest/rest.service.spec.ts
+++ src/server/common/rest/rest.service.spec.ts
@@ -5,11 +5,14 @@ import { FakeConfigModule } from '../../config/config.fake'
 import { ApiConfig } from '../../config'
 import { ConfigService } from '@nestjs/config'
 import { fakeUser } from '../../security/user/user.fake'
-import { SinonStubbedInstance, createStubInstance } from 'sinon'
+import { createStubInstance, SinonStubbedInstance } from 'sinon'
 import { HmppsOidcService } from '../hmpps-oidc/hmpps-oidc.service'
-import { Logger } from '@nestjs/common'
+import { HttpStatus, Logger } from '@nestjs/common'
 import { MockLoggerModule } from '../../logger/logger.mock'
 import { SanitisedAxiosError } from './SanitisedAxiosError'
+import { omit } from 'lodash'
+
+nock.disableNetConnect()
 
 const URL = '/some-url'
 const OK = Object.freeze({ hello: 'world' })
@@ -35,13 +38,30 @@ describe('RestService', () => {
     config = module.get(ConfigService).get('apis.community')
   })
 
+  it('does not retry 404', async () => {
+    nock(config.url.href)
+      .matchHeader('authorization', `Bearer ${user.token}`)
+      .get(URL)
+      .reply(404, { developerMessage: 'this is not found' })
+
+    expect.assertions(3)
+    await subject
+      .build('community', user)
+      .get(URL)
+      .catch((err: SanitisedAxiosError) => {
+        expect(err).toBeInstanceOf(SanitisedAxiosError)
+        expect(err.response?.status ?? 'no response').toBe(HttpStatus.NOT_FOUND)
+        expect(err.retries).toBe(0)
+      })
+  })
+
   it('succeeds without retry', async () => {
     nock(config.url.href).matchHeader('authorization', `Bearer ${user.token}`).get(URL).reply(200, OK)
     const observed = await subject.build('community', user).get(URL)
     expect(observed.data).toEqual(OK)
   })
 
-  it('succeeds after single retry', async () => {
+  it('succeeds after retry', async () => {
     const bad = nock(config.url.href).matchHeader('authorization', `Bearer ${user.token}`).get(URL).reply(500)
     const ok = bad.get(URL).reply(200, OK)
     const observed = await subject.build('community', user).get(URL)
@@ -50,41 +70,38 @@ describe('RestService', () => {
     expect(ok.isDone()).toBe(true)
   })
 
-  it('fails after 3 retries', async () => {
+  it('fails after retry', async () => {
     nock(config.url.href)
       .matchHeader('authorization', `Bearer ${user.token}`)
       .get(URL)
       .reply(500)
       .get(URL)
-      .reply(500)
-      .get(URL)
-      .reply(500)
-      .get(URL)
       .reply(500, { error: 'some error' })
 
-    let exception: SanitisedAxiosError
-    try {
-      await subject.build('community', user).get(URL)
-    } catch (err) {
-      exception = err
-    }
-
-    expect({ ...exception, message: exception.message }).toEqual({
-      message: 'Request failed with status code 500',
-      api: {
-        baseUrl: config.url.href,
-        name: 'community',
-      },
-      name: 'SanitisedAxiosError',
-      request: {
-        method: 'GET',
-        url: URL,
-      },
-      response: {
-        data: { error: 'some error' },
-        status: 500,
-        statusText: null,
-      },
-    })
+    expect.assertions(2)
+    await subject
+      .build('community', user)
+      .get(URL)
+      .catch((err: SanitisedAxiosError) => {
+        expect({ ...omit(err, 'responseTime'), message: err.message }).toEqual({
+          message: 'Request failed with status code 500',
+          api: {
+            baseUrl: config.url.href,
+            name: 'community',
+          },
+          name: 'SanitisedAxiosError',
+          request: {
+            method: 'GET',
+            url: URL,
+          },
+          response: {
+            data: { error: 'some error' },
+            status: 500,
+            statusText: null,
+          },
+          retries: 1,
+        })
+        expect(err.responseTime).toBeGreaterThanOrEqual(0)
+      })
   })
 })
