725b4dfe68543bc4aa1a733d0c487f255582cabe
dnyanesh.sonwane <dnyanesh.sonwane@digital.justice.gov.uk>
2021-11-29 10:44:58 +0000

    trello-983-add-notes-pages

--- cypress/integration/record-outcome/record-outcome.spec.ts
+++ cypress/integration/record-outcome/record-outcome.spec.ts
@@ -28,12 +28,6 @@ context('Record outcome happy path & validation', () => {
       .shouldDisplayEnforcementErrors('There is a problem', 'Select an enforcement')
       .whenSelectingEnforcementAction('Refer to Offender Manager')
       .whenSubmittingCurrentStep()
-      .shouldDisplayEnforcementPage()
-      .whenSubmittingCurrentStep()
-      // testing the validation
-      .shouldDisplayEnforcementErrors('There is a problem', 'Select an enforcement')
-      .whenSelectingEnforcementAction('Refer to Offender Manager')
-      .whenSubmittingCurrentStep()
   })
   it('can record outcome - failed to attend', () => {
     new RecordOutcomeFixture()
